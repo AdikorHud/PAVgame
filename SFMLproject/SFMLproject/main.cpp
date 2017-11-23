d1da6f768483a77eacc7ffc3eac9d00a540dccb8
AdikorHud <leandrosena75@gmail.com>
2017-11-22 22:28:00 -0300

    -Test for loading textures (temporary)

--- SFMLproject/SFMLproject/main.cpp
+++ SFMLproject/SFMLproject/main.cpp
@@ -1,11 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include <vld.h>
+#include <iostream>
 
 int main()
-{
+{	
+	sf::Texture myTexture;
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Game on!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Yellow);
 	
+	sf::Sprite mySheet;
+	mySheet.setTexture(myTexture);
+
+	if (!myTexture.loadFromFile("Assets/Spritesheet/sheet_characters.png"))
+	{
+		std::cout << "Error! No encontre una textura";
+	}
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -15,8 +26,11 @@ int main()
 				window.close();
 		}
 
+		
+
 		window.clear();
-		window.draw(shape);
+		window.draw(mySheet);
+		//window.draw(shape);
 		window.display();
 	}
 
