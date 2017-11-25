b7fcf3cd0dca159f290a0968340bf41349e4e2f4
AdikorHud <leandrosena75@gmail.com>
2017-11-25 12:39:08 -0300

    -Test for loading textures completed. -Test for loading audio in progress.

--- SFMLproject/SFMLproject/main.cpp
+++ SFMLproject/SFMLproject/main.cpp
@@ -1,21 +1,41 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#ifdef DEBUG
 #include <vld.h>
+#endif // DEBUG
 #include <iostream>
 
 int main()
 {	
-	sf::Texture myTexture;
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Game on!");
+		
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Yellow);
 	
-	sf::Sprite mySheet;
-	mySheet.setTexture(myTexture);
-
-	if (!myTexture.loadFromFile("Assets/Spritesheet/sheet_characters.png"))
+	sf::Texture myTexture;
+	sf::Sprite blueChar;
+		
+	
+	if (!myTexture.loadFromFile("Assets/PNG/Blue/characterBlue (1).png"))
 	{
 		std::cout << "Error! No encontre una textura";
 	}
+
+	blueChar.setTexture(myTexture);
+
+
+	sf::SoundBuffer myBuffer;
+	sf::Sound mySound;
+
+	if (!myBuffer.loadFromFile("Assets/Audio/click3.wav"))
+	{
+		std::cout << "Error! No encontre un audio";
+		//return -1;
+	}
+	
+	mySound.setBuffer(myBuffer);
+	mySound.play();
+
 
 	while (window.isOpen())
 	{
@@ -24,12 +44,10 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-		}
-
+		}		
 		
-
 		window.clear();
-		window.draw(mySheet);
+		window.draw(blueChar);
 		//window.draw(shape);
 		window.display();
 	}
