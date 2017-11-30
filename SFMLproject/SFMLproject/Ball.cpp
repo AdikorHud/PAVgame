97150d9e896ab73a3e0fd10b66fb85490bbf563b
AdikorHud <leandrosena75@gmail.com>
2017-11-29 22:00:10 -0300

    -Vector3 added to Ball class

--- SFMLproject/SFMLproject/Ball.cpp
+++ SFMLproject/SFMLproject/Ball.cpp
@@ -1,12 +1,26 @@
 #include "Ball.h"
+#include "Character.h"
 
 
 
 Ball::Ball()
+	:
+	ballVelocity()
 {
+}
+
+Ball::Ball(float x, float y, float z, Character player)
+{
+	ballVelocity.x = x;
+	ballVelocity.y = y;
+	ballVelocity.z = y;
 }
 
 
 Ball::~Ball()
+{
+}
+
+void Ball::UpdateMovement()
 {
 }
--- SFMLproject/SFMLproject/Ball.h
+++ SFMLproject/SFMLproject/Ball.h
@@ -1,8 +1,17 @@
 #pragma once
+#include <SFML\System\Vector3.hpp>
+#include <SFML\Graphics.hpp>
+
 class Ball
 {
+private:
+	sf::Vector3<float> ballVelocity;
+
+
 public:
 	Ball();
+	Ball(float x, float y, float z, Character player);
 	~Ball();
-};
 
+	void UpdateMovement();
+};
